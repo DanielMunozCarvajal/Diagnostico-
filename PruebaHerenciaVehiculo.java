75e0c89b30719d0d18b63ce2520594559406d4f0
DanielMunozCarvajal <110427980+DanielMunozCarvajal@users.noreply.github.com>
2022-08-22 14:05:13 -0600

    Add files via upload

---
+++ PruebaHerenciaVehiculo.java
@@ -0,0 +1,17 @@
+
+package com.mycompany.prueba.herencia.vehiculo;
+
+public class PruebaHerenciaVehiculo {
+
+    public static void main(String[] args) {
+        System.out.println("-----Bienvenido----- \n");
+        System.out.println("Digite el numero del vehiculo que quiere agregar a la lista: \n");
+        System.out.println("1- Coche \n");
+        System.out.println("2- Bicicleta \n");
+        System.out.println("3- Camioneta \n");
+        System.out.println("4- Motocicleta \n");
+        String selecion = ""; 
+       
+        PruebaHerenciaVehiculo Coche = new Coche(); 
+    }
+}
---
+++ monto_neto.java
@@ -0,0 +1,8 @@
+
+package com.mycompany.factura;
+
+
+public class monto_neto extends pagos{
+    
+    
+}
